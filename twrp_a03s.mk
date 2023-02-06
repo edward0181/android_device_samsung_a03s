9c2908ad8934af5669a6239101ee5a65b5c61a58
Edward0181 <edwardroggeveen@gmail.com>
2023-02-06 10:06:05 +0100

    first commit

---
+++ twrp_a03s.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := a03s
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Inherit some common TWRP stuff.
+# For PitchBlack use vendor/pb/config/common.mk
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/a03s/device.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a03s/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_a03s
+PRODUCT_DEVICE := a03s
+PRODUCT_MODEL := Galaxy A03s
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 ItzKaguya at ShirakawaProject
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo twrp_a03s-eng
